f052605d8dfd995096d4ea321182014ba22e9043
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-08 17:33:00 -0300

    Add files via upload

---
+++ incre-decre.js
@@ -0,0 +1,25 @@
+/*Criar uma variável e incrementar
+  Criar outra variável e decrementar 
+  Criar uma variável com um Number 
+  Criar uma variável que será atribuido o valor da primeira variável e fazer a incrementação
+  Criar uma variável que será atribuido o valor da primeira variável e fazer a decrementação*/
+
+  //incrementando 
+ var a = 1;
+ a++;
+ console.log(a);
+
+  //Decrementando
+ var b = 2;
+ b--;
+ console.log(b);
+
+  //incrementando por outra variável
+ var c = 33;
+ var d = ++c;
+ console.log(d);
+
+  //Decrementando por outra variável
+ var e = 32;
+ var f = --e;
+ console.log(f);
